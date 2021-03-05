c43afe7efe4c081f427df822c6de061b1fd3795a
Michael Stephens <mdstephens26@yahoo.com>
2021-03-04 22:17:21 -0500

    Added Users table

---
+++ db/migrate/20210304235253_create_users_table.rb
@@ -0,0 +1,11 @@
+class CreateUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :user_name
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+    end
+  end
+end
